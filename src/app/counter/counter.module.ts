50a6e91f1434f9326fed3bdfd8d22d51b8501fec
Oswaldo Rios <chomper.or@gmail.com>
2025-01-28 00:29:37 -0600

    Fin de seccion 4

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { CounterComponent } from "./Components/Counter/counter.component";
+
+@NgModule({
+    declarations:[
+        CounterComponent
+    ],
+    exports:[
+        CounterComponent
+    ]
+})
+export class CounterModule{}
